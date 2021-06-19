b8dc0f37bc00d4a024006199325431153746ba6c
sukhdeep11 <sukhdeep.rayat1@gmail.com>
2021-06-19 12:13:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PhotoSectionComponent } from './components/photo-section/photo-section.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    HomeComponent,
+    FooterComponent,
+    PhotoSectionComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/photo-section/photo-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhotoSectionComponent } from './photo-section.component';
+
+describe('PhotoSectionComponent', () => {
+  let component: PhotoSectionComponent;
+  let fixture: ComponentFixture<PhotoSectionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PhotoSectionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhotoSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
